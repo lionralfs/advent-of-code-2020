a266a4d665f23d18218895891789e4219d65f875
Lion Ralfs <lion.ralfs@gmail.com>
2020-12-01 06:23:11 +0100

    [day01] complete with tests

--- day01/main.go
+++ day01/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func partOne() {
@@ -12,9 +14,49 @@ func partOne() {
 	}
 	defer file.Close()
 
-	result := 0
+	scanner := bufio.NewScanner(file)
+	list := make([]int, 0)
 
-	fmt.Printf("[Part1]: %v\n", result)
+	for scanner.Scan() {
+		mass, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+		list = append(list, mass)
+	}
+
+	fmt.Printf("[Part1]: %v\n", twoSummands(list))
+}
+
+func twoSummands(nums []int) int {
+	for i, val1 := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			val2 := nums[j]
+
+			if val1+val2 == 2020 {
+				return val1 * val2
+			}
+		}
+	}
+
+	return -1
+}
+
+func threeSummands(nums []int) int {
+	for i, val1 := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			val2 := nums[j]
+			for k := j + 1; k < len(nums); k++ {
+				val3 := nums[k]
+
+				if val1+val2+val3 == 2020 {
+					return val1 * val2 * val3
+				}
+			}
+		}
+	}
+
+	return -1
 }
 
 func partTwo() {
@@ -24,9 +66,18 @@ func partTwo() {
 	}
 	defer file.Close()
 
-	result := 0
+	scanner := bufio.NewScanner(file)
+	list := make([]int, 0)
 
-	fmt.Printf("[Part2]: %v\n", result)
+	for scanner.Scan() {
+		mass, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+		list = append(list, mass)
+	}
+
+	fmt.Printf("[Part2]: %v\n", threeSummands(list))
 }
 
 func main() {
--- day01/main_test.go
+++ day01/main_test.go
@@ -2,20 +2,38 @@ package main
 
 import "testing"
 
-func TestDummyFunc(t *testing.T) {
+func TestPart1(t *testing.T) {
 	tests := []struct {
-		input int
+		input []int
 		want  int
 	}{
 		{
-			input: 0,
-			want:  0,
+			input: []int{1721, 979, 366, 299, 675, 1456},
+			want:  514579,
 		},
 	}
 
 	for _, test := range tests {
-		if output := 0; output != test.want {
-			t.Errorf("mydummyfunc(%d) = %v; want %d", test.input, output, test.want)
+		if output := twoSummands(test.input); output != test.want {
+			t.Errorf("twoSummands(%d) = %v; want %d", test.input, output, test.want)
+		}
+	}
+}
+
+func TestPart2(t *testing.T) {
+	tests := []struct {
+		input []int
+		want  int
+	}{
+		{
+			input: []int{1721, 979, 366, 299, 675, 1456},
+			want:  241861950,
+		},
+	}
+
+	for _, test := range tests {
+		if output := threeSummands(test.input); output != test.want {
+			t.Errorf("twoSummands(%d) = %v; want %d", test.input, output, test.want)
 		}
 	}
 }
